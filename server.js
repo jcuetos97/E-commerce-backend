e6425a04526af32e37de26cbd0fc6fb31bb20c3b
jcuetos97 <105081149+jcuetos97@users.noreply.github.com>
2022-09-12 10:40:30 -0500

    models created, index.js updated

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
+const sequelize = require('./config/connection'); 
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Express middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Turn on routes
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+// Turn on connection to database and server
+sequelize.sync({ force:false }).then(()=>{
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+})
+
+
+
